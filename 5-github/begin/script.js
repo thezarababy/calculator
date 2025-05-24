a602072b55f01110dc8aeaf8f8981ea77c277a8b
thezarababy <jyokoduwa@gmail.com>
2025-05-24 12:43:25 +0100

    javascript

---
+++ 5-github/begin/script.js
@@ -0,0 +1,12 @@
+let display = document.getElementById("display");
+let clear = document.getElementById("clear");
+let goBack = document.getElementById("goBack");
+let divide = document.getElementById("divide");
+let seven = document.getElementById("seven")
+function inc(){
+    display.innerHTML=0;
+}
+function num7 (){
+display.innerHTML= 7;
+}
+
