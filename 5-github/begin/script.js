b6341509977459b8fa09a00dab36c4eb9e2cc6ec
thezarababy <jyokoduwa@gmail.com>
2025-05-27 16:34:03 +0100

    eval

--- 5-github/begin/script.js
+++ 5-github/begin/script.js
@@ -1,12 +1,35 @@
 let display = document.getElementById("display");
-let clear = document.getElementById("clear");
-let goBack = document.getElementById("goBack");
-let divide = document.getElementById("divide");
-let seven = document.getElementById("seven")
-function inc(){
-    display.innerHTML=0;
-}
-function num7 (){
-display.innerHTML= 7;
+
+
+function toDisplay(input){
+    display.value+=input
 }
 
+function toClear(){
+    display.value="";
+
+}
+ function  toCalculate(){
+    try{
+     let result=eval(display.value);
+     display.value= result;
+    }
+     catch(error){
+        display.value ='Error';
+       }
+ }
+ function goBack(){
+    display.value = display.value.slice(0,-1);
+ }
+
+
+ // let clear = document.getElementById("clear");
+// let goBack = document.getElementById("goBack");
+// let divide = document.getElementById("divide");
+// let seven = document.getElementById("seven")
+// function inc(){
+//     display.innerHTML=0;
+// }
+// function num7 (){
+// display.innerHTML= 7;
+// }
